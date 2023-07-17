c309250e40b67b6f6483bdc9f1f84a1b124c8265
Anthony Gargano <anthonyg92297@gmail.com>
2023-07-16 22:46:21 -0700

    Pull pybaseball into its own script. Testing Github actions since Vercel serverless functions can't be >50MB.

--- api/index.py
+++ api/index.py
@@ -1,48 +1,33 @@
 import json
-from timedlrucache import timed_lru_cache
 
 import jsonpickle
 import pandas as pd
 from espn_api.baseball import League
 from flask import Flask
-from pybaseball import batting_stats, pitching_stats
 from unidecode import unidecode
 
-from constants import (CURRENT_YEAR, FANGRAPHS_BATTING_FIELDS,
-                       FANGRAPHS_PITCHING_FIELDS, USELESS_POSITIONS,
-                       lineupSlotOrder)
+from constants import (
+    USELESS_POSITIONS,
+    lineupSlotOrder,
+)
 
 app = Flask(__name__)
 
-@app.route('/api')
-def home():
-    return 'Hello, World!'
-
-@app.route('/api/getPlayerData')
+@app.route("/api/getPlayerData")
 def getPlayerData():
-    batterStats = getBatterStats()
-    pitcherStats = getPitcherStats()
+    fangraphsStats = pd.read_pickle("../pybaseballdata/playerdata.txt")
 
     league = League(league_id=167157, year=2023)
 
-    playerData = combineFantasyAndStatsData(
-        league, batterStats, pitcherStats
-    )
+    playerData = combineFantasyAndStatsData(league, fangraphsStats)
 
     return json.dumps(playerData.to_dict("records")).replace("NaN", "null")
 
-@app.route('/api/getLeagueInfo')
+
+@app.route("/api/getLeagueInfo")
 def getLeagueInfo():
     league = League(league_id=167157, year=2023)
     return jsonpickle.dumps(league)
-
-@timed_lru_cache(seconds=(60 * 60 * 6))
-def getBatterStats():
-    return batting_stats(CURRENT_YEAR, qual=20)[FANGRAPHS_BATTING_FIELDS]
-
-@timed_lru_cache(seconds=(60 * 60 * 6))
-def getPitcherStats():
-    return pitching_stats(CURRENT_YEAR, qual=20)[FANGRAPHS_PITCHING_FIELDS]
 
 
 def getStatsForFantasyTeam(teamName, roster, playerStats):
@@ -68,40 +53,21 @@ def getStatsForFantasyTeam(teamName, roster, playerStats):
     )
 
 
-def filterPlayersByPosition(players, position):
-    return list(filter(lambda player: position in player.eligibleSlots, players))
-
-
-def combineFantasyAndStatsData(league, batterStats, pitcherStats):
-    batterData = getStatsForFantasyTeam(
-        "Free Agent", filterPlayersByPosition(league.free_agents(), "UTIL"), batterStats
-    )
-    pitcherData = getStatsForFantasyTeam(
-        "Free Agent", filterPlayersByPosition(league.free_agents(), "P"), pitcherStats
+def combineFantasyAndStatsData(league, fangraphsStats):
+    playerData = getStatsForFantasyTeam(
+        "Free Agent", league.free_agents(), fangraphsStats
     )
     for team in league.teams:
-        batterData = pd.concat(
+        playerData = pd.concat(
             [
-                batterData,
+                playerData,
                 getStatsForFantasyTeam(
                     team.team_name,
-                    filterPlayersByPosition(team.roster, "UTIL"),
-                    batterStats,
+                    team.roster,
+                    fangraphsStats,
                 ),
             ],
-            ignore_index=True
+            ignore_index=True,
         )
-        pitcherData = pd.concat(
-            [
-                pitcherData,
-                getStatsForFantasyTeam(
-                    team.team_name,
-                    filterPlayersByPosition(team.roster, "P"),
-                    pitcherStats,
-                ),
-            ],
-            ignore_index=True
-        )
-    data = pd.concat([batterData, pitcherData], ignore_index=True)
-    data['id'] = data.index
-    return data
+    playerData["id"] = playerData.index
+    return playerData
---
+++ pybaseballdata/update_pybaseball_data.py
@@ -0,0 +1,29 @@
+import json
+
+import pandas as pd
+from pybaseball import batting_stats, pitching_stats
+
+from constants import (CURRENT_YEAR, FANGRAPHS_BATTING_FIELDS,
+                       FANGRAPHS_PITCHING_FIELDS)
+
+def getPlayerData():
+    batterStats = getBatterStats()
+    pitcherStats = getPitcherStats()
+
+    playerData = combineFantasyAndStatsData(batterStats, pitcherStats)
+
+    return playerData.to_pickle("playerdata.txt")
+
+def getBatterStats():
+    return batting_stats(CURRENT_YEAR, qual=20)[FANGRAPHS_BATTING_FIELDS]
+
+def getPitcherStats():
+    return pitching_stats(CURRENT_YEAR, qual=20)[FANGRAPHS_PITCHING_FIELDS]
+
+def combineFantasyAndStatsData(batterStats, pitcherStats):
+    data = pd.concat([batterStats, pitcherStats], ignore_index=True)
+    data['id'] = data.index
+    return data
+
+if __name__ == "__main__":
+    getPlayerData()
